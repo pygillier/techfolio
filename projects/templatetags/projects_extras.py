37cd185217e4acbf826b359f798d0590dec46dd5
Pierre-Yves G <pygillier@users.noreply.github.com>
2019-04-07 17:12:52 +0200

    Projects

---
+++ projects/templatetags/projects_extras.py
@@ -0,0 +1,17 @@
+from django import template
+from django.utils.safestring import mark_safe
+from ..models import PlatformChoice
+
+
+register = template.Library()
+
+@register.filter(name='platform_icon', is_safe=True)
+def get_platform_icon(platform_id):
+    if platform_id == PlatformChoice.GITHUB:
+        icon = "github"
+    elif platform_id == PlatformChoice.GITLAB:
+        icon = "gitlab"
+    else:
+        icon = "question-circle"
+
+    return mark_safe(f'<i class="fab fa-{icon}"></i>')
